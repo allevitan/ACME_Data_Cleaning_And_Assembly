Fix the rotation of the detector

src/acme_data_cleaning/image_handling.py
@@ -107,7 +107,9 @@ def map_tiles_to_frame(tiles, include_overscan=False):
     bottom_half = np.concatenate(bottom_tiles.swapaxes(-3,0), -1)
     bottom_half = bottom_half[...,::-1,::-1]
 
-    return np.concatenate((top_half, bottom_half), axis=-2)
+    frame = np.concatenate((top_half, bottom_half), axis=-2)
+    #The last thing which needs to be done is a rotation 
+    return frame.swapaxes(-1,-2)[...,::-1,::-1]
 
 
 @partial(np.vectorize, signature='(n,m),(k,l)->(n,m)')

src/acme_data_cleaning/process_stxm_file.py
@@ -105,7 +105,9 @@ def main(argv=sys.argv):
     # Default mask, TODO: should be loaded from a file
     default_mask = np.zeros([960,960])
     default_mask = default_mask.at[:480,840:].set(1)
-
+    default_mask = default_mask.at[:480,590].set(1)
+    default_mask = default_mask.swapaxes(-1,-2)[...,::-1,::-1]
+    
     for stxm_filename in stxm_filenames:
         output_filename = '.'.join(stxm_filename.split('.')[:-1])+'.cxi'
         if not args.succinct: