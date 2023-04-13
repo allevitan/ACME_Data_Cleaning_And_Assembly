da27d9116becd688d16014577d57451744c03d87
Silvio Achilles <silvio.achilles@desy.de>
2023-04-13 14:03:26 -0700

    Added initialization of illumination and illumination mask to process_live_data

---
+++ src/acme_data_cleaning/illumination_init.py
@@ -0,0 +1,74 @@
+import numpy as np
+from scipy import constants
+
+
+def create_illumination(dps):
+    dataAve = dps.mean(0)
+    pMask = np.fft.fftshift((dataAve > 0.01 * dataAve.max()))
+    probe = np.sqrt(np.fft.fftshift(dataAve)) * pMask
+    probe = np.fft.ifftshift(np.fft.ifftn(probe))
+    return probe, pMask
+
+
+def propnf(a, z, l):
+    """
+    propnf(a,z,l) where z and l are in pixel units
+    """
+
+    if a.ndim != 2:
+        raise RuntimeError("A 2-dimensional wave front 'w' was expected")
+    # if a.shape[0] != a.shape[1]:
+    #    raise RunTimeError("Only square arrays are currently supported")
+
+    n = len(a)
+    ny, nx = a.shape
+    qx, qy = np.meshgrid(np.linspace(-nx / 2, nx / 2, nx), np.linspace(-ny / 2, ny / 2, ny))
+    q2 = np.fft.fftshift(qx ** 2 + qy ** 2)
+
+    if n / np.sqrt(2.) < np.abs(z) * l:
+        print("Warning: %.2f < %.2f: this calculation could fail." % (n / np.sqrt(2.), np.abs(z) * l))
+        print("You could enlarge your array, or try the far field method: propff()")
+
+    return np.fft.ifftn(np.fft.fftn(a) * np.exp(-2j * np.pi * (z / l) * (np.sqrt(1 - q2 * (l / n) ** 2) - 1)))
+
+
+def defocus_illumination(probe, energy_joule, px_size_nm):
+    probe_defocused = probe.copy()
+    energy_eV = energy_joule / constants.e
+    # l = 1240. / energy_eV
+    l = constants.h * constants.c / energy_joule
+    l_nm = l * 1e9
+    defocus = 15. * energy_eV / 700.
+    probe_defocused = propnf(probe_defocused, defocus * 1000. / px_size_nm, l_nm / px_size_nm)
+    return probe_defocused
+
+
+def calculate_real_space_px_size(energy_joule, d_sample_detector, npx_detector, d_px_detector):
+    wavelength = constants.h * constants.c / energy_joule
+    d_detector = npx_detector * d_px_detector
+    alpha = np.arctan(d_detector / (2 * d_sample_detector))
+    px_size = wavelength / (2 * np.sin(alpha))
+    return px_size
+
+
+def init_illumination(dps, metadata):
+    energy_joule = metadata['energy']
+    energy_ev = energy_joule / constants.e
+
+    probe, probe_mask = create_illumination(dps)
+
+    # if energy_ev != 700:
+    distance = metadata['detector_distance']
+    d_px_detector = metadata['x_pixel_size']
+    npx_detector = metadata['output_frame_width']
+    px_size_m = calculate_real_space_px_size(
+        energy_joule,
+        distance,
+        npx_detector,
+        d_px_detector
+    )
+    px_size_nm = px_size_m * 1e9
+
+    probe = defocus_illumination(probe, energy_joule, px_size_nm)
+
+    return probe, probe_mask
--- src/acme_data_cleaning/process_live_data.py
+++ src/acme_data_cleaning/process_live_data.py
@@ -1,14 +1,16 @@
+import time
 import os
 import zmq
 import h5py
 from contextlib import contextmanager
-from acme_data_cleaning import image_handling, file_handling, config_handling
+from acme_data_cleaning import image_handling, file_handling, config_handling, illumination_init
 import argparse
 import sys
 import torch as t
 import numpy as np
 from cosmicstreams.PreprocessorStream import PreprocessorStream
 from scipy import constants
+import copy
 
 
 # How should I structure this? I'll get 4 kinds of events, and each event
@@ -42,7 +44,8 @@ def make_new_state():
         'position': None,
         'frame_cleaner': None,
         'resampler': None,
-        'mask': None
+        'mask': None,
+        'start_sending_frames': False
     }
 
 
@@ -53,8 +56,6 @@ def make_new_state():
 def process_start_event(state, event, pub, config):
     """Processes a start event, getting the metadata and opening a cxi_file
     """
-    # pub.send_pyobj(event)
-
     print('Processing start event')
 
     # We load the mask, preloaded on the correct device
@@ -87,58 +88,51 @@ def process_start_event(state, event, pub, config):
     state['darks'] = {dwell: None for dwell in state['dwells']}
     state['n_darks'] = {dwell: 0 for dwell in state['dwells']}
     state['pix_translations'] = True
+    # state['metadata']['pix_translations'] = True
 
     # state['metadata']['translations'] = (-np.array(state['metadata']['translations']) * 1e-6).tolist()
     state['metadata']['translations'] = (np.array(state['metadata']['translations']) * 1e-6).tolist()
-    metadata_cxi = state['metadata']
-    metadata_cxi['output_frame_width'] = config['output_pixel_count']
-    metadata_cxi['x_pixel_size'] = psize
-    metadata_cxi['y_pixel_size'] = psize
-    metadata_cxi['detector_distance'] = state['metadata']['geometry']['distance']
-    metadata_cxi['energy'] = state['metadata']['energy'] * constants.e
-    metadata_cxi['pix_translations'] = True
+    state['metadata'] = state['metadata']
+    # state['metadata'] = copy.deepcopy(state['metadata'])
+    state['metadata']['output_frame_width'] = config['output_pixel_count']
+    state['metadata']['x_pixel_size'] = psize
+    state['metadata']['y_pixel_size'] = psize
+    state['metadata']['detector_distance'] = state['metadata']['geometry']['distance']
+    state['metadata']['energy'] = state['metadata']['energy'] * constants.e
+    state['metadata']['pix_translations'] = True
 
-    wavelength = constants.h * constants.c / metadata_cxi['energy']
-    alpha = np.arctan(psize * metadata_cxi['output_frame_width'] / (2 * metadata_cxi['detector_distance']))
+    wavelength = constants.h * constants.c / state['metadata']['energy']
+    alpha = np.arctan(psize * state['metadata']['output_frame_width'] / (2 * state['metadata']['detector_distance']))
     px_size = wavelength / (2 * np.sin(alpha))
     state['px_size_real_space'] = px_size
 
     # Translations are originally given as (y, x).
-    metadata_cxi['translations'] = np.array(metadata_cxi['translations'])
-    translations_tmp = metadata_cxi['translations'].copy()
+    state['metadata']['translations'] = np.array(state['metadata']['translations'])
+    translations_tmp = state['metadata']['translations'].copy()
 
     # Translations are changed to (x, y) to apply the shear.
-    metadata_cxi['translations'][:, 0] = translations_tmp[:, 1]
-    metadata_cxi['translations'][:, 1] = translations_tmp[:, 0]
-    metadata_cxi['translations'] = np.dot(config['shear'], metadata_cxi['translations'].T).T
+    state['metadata']['translations'][:, 0] = translations_tmp[:, 1]
+    state['metadata']['translations'][:, 1] = translations_tmp[:, 0]
+    state['metadata']['translations'] = np.dot(config['shear'], state['metadata']['translations'].T).T
 
-    # translations_tmp = metadata_cxi['translations'].copy()
-    # metadata_cxi['translations'][:, 0] = translations_tmp[:, 1]
-    # metadata_cxi['translations'][:, 1] = translations_tmp[:, 0]
-    metadata_cxi['translations'] = np.array(metadata_cxi['translations']) / px_size
-    # translations_tmp = metadata_cxi['translations'].copy()
-    # metadata_cxi['translations'][:, 0] = translations_tmp[:, 1]
-    # metadata_cxi['translations'][:, 1] = translations_tmp[:, 0]
-    # metadata_cxi['translations'] = np.dot(config['shear'], metadata_cxi['translations'].T).T
-    # translations_tmp = metadata_cxi['translations'].copy()
-    # metadata_cxi['translations'][:, 0] = translations_tmp[:, 1]
-    # metadata_cxi['translations'][:, 1] = translations_tmp[:, 0]
-    metadata_cxi['translations'] = np.ceil(metadata_cxi['translations'])
-    metadata_cxi['translations'][:, 0] -= metadata_cxi['translations'][:, 0].min()
-    metadata_cxi['translations'][:, 1] -= metadata_cxi['translations'][:, 1].min()
+    state['metadata']['translations'] = np.array(state['metadata']['translations']) / px_size
+    state['metadata']['translations'] = np.ceil(state['metadata']['translations'])
+    state['metadata']['translations'][:, 0] -= state['metadata']['translations'][:, 0].min()
+    state['metadata']['translations'][:, 1] -= state['metadata']['translations'][:, 1].min()
 
-    metadata_cxi['translations'] = metadata_cxi['translations'].tolist()
+    state['metadata']['translations'] = state['metadata']['translations'].tolist()
 
-    # metadata_cxi = file_handling.transform_metadata_acme_to_cxi(state['metadata'])
     identifier = get_dataset_name(state['metadata'])
-    metadata_cxi['identifier'] = identifier
+    state['metadata']['identifier'] = identifier
 
-    pub.send_start(metadata_cxi)
+    metadata_cxi = copy.deepcopy(state['metadata'])
+    state['metadata_cxi'] = metadata_cxi
+    # pub.send_start(metadata_cxi)
 
     # Now I find the right filename to save the .cxi file in
     output_filename = make_output_filename(state)
     print('Saving data to', output_filename)
-    
+
     # Next, I need to create and leave open that .cxi file
     # Energy has to be in eV
     state['metadata']['energy'] = state['metadata']['energy'] / constants.e
@@ -161,7 +155,7 @@ def make_output_filename(state):
         energy_no = 0
 
     return header + '_ccdframes_%d_%d.cxi' % (energy_no, region_no)
-    
+
 
 def process_dark_event(cxi_file, state, event, config):
     print('Processing dark event')
@@ -175,13 +169,13 @@ def process_dark_event(cxi_file, state, event, config):
         state['darks'][dwell] = dark
     else:
         n_darks = state['n_darks'][dwell]
-        state['darks'][dwell] = ((state['darks'][dwell] * n_darks + dark) / 
+        state['darks'][dwell] = ((state['darks'][dwell] * n_darks + dark) /
                                  (n_darks + 1))
     state['n_darks'][dwell] += 1
 
 
 def process_exp_event(cxi_file, state, event, pub, config):
-    print('Processing exp event',event['data']['index'])
+    print('Processing exp event', event['data']['index'])
 
     if state['frame_cleaner'] is None:
         print('Creating the frame cleaner; all further darks will be disregarded')
@@ -189,59 +183,63 @@ def process_exp_event(cxi_file, state, event, pub, config):
             image_handling.FastCCDFrameCleaner(
                 [state['darks'][dwell] for dwell in state['dwells']])
 
-    # state['position'] = np.array([-event['data']['xPos'],
-    #                                -event['data']['yPos']]) * 1e-6
-    state['position'] = np.array([event['data']['xPos'],
-                                  event['data']['yPos']]) * 1e-6 / state['px_size_real_space']
-    
+    # state['position'] = np.array([-event['data']['xPos'], -event['data']['yPos']]) * 1e-6
+    state['position'] = np.array([event['data']['xPos'], event['data']['yPos']]) * 1e-6 / state['px_size_real_space']
+
     # A correction for the shear in the probe positions
     state['position'] = np.matmul(config['shear'], state['position'])
 
+    frame_numpy = None
     if event['data']['index'] != state['index']:
         # we've moved on, so we need to finalize the frame
-        finalize_frame(cxi_file, state, pub, config)
+        frame_numpy = finalize_frame(cxi_file, state, pub, config)
         state['index'] = event['data']['index']
 
     dwell = event['data']['dwell']
     dwell_idx = np.where(state['dwells'] == dwell)[0][0]
-    
-    raw_frame = t.as_tensor(np.array(event['data']['ccd_frame'],
-                                     dtype=np.float32),
-                            device=config['device'])
+
+    raw_frame = t.as_tensor(
+        np.array(event['data']['ccd_frame'], dtype=np.float32),
+        device=config['device']
+    )
+
     frame, mask = state['frame_cleaner'].process_frame(
-        raw_frame, dwell_idx, include_overscan=False,
+        raw_frame,
+        dwell_idx,
+        include_overscan=False,
         med_width=config['background_median_width'],
         max_correction=config['max_overscan_correction'],
         min_correction=config['min_overscan_correction'],
         background_offset=config['background_offset'],
-        cut_zeros=config['cut_zeros'])
+        cut_zeros=config['cut_zeros']
+    )
 
     # We always need to do this
     state['current_exposures'][dwell].append(frame)
     state['current_masks'][dwell].append(mask)
 
+    return frame_numpy
+
 
 def finalize_frame(cxi_file, state, pub, config):
-
     # we first combine the frames
-    
+
     all_dwells = []
     all_frames = []
     all_masks = []
     for dwell, frames in state['current_exposures'].items():
         masks = state['current_masks'][dwell]
-        all_dwells.extend([dwell,] * len(frames))
+        all_dwells.extend([dwell, ] * len(frames))
         all_frames.extend(frames)
         all_masks.extend(masks)
 
-
     if len(all_frames) == 0:
         return
-    
+
     all_dwells = np.array(all_dwells)
     all_frames = t.stack(all_frames, dim=0)
     all_masks = t.stack(all_masks, dim=0)
-    
+
     synthesized_frame, synthesized_mask = image_handling.combine_exposures(
         all_frames, all_masks, all_dwells)
 
@@ -260,36 +258,26 @@ def finalize_frame(cxi_file, state, pub, config):
         # And, one final correction, we need to update the pixel sizes on
         file_handling.update_cxi_metadata(cxi_file, state['metadata'])
 
-
     resampled_frame, resampled_mask = \
         state['resampler'].resample(synthesized_frame,
                                     masks=synthesized_mask)
 
-        
     numpy_resampled_frame = resampled_frame.cpu().numpy()
 
-    basis = np.array(state['metadata']['geometry']['basis_vectors']).transpose()
-    output_event = {
-        'event':'frame',
-        'data': numpy_resampled_frame,
-        'position': state['position'],
-        'basis': basis
-    }
-
-    # identifier, data, index, posy, posx, metadata
-    pos_x, pos_y = state['position']
-    filepath = state['metadata']['header']
-    filename = os.path.basename(filepath)
-    identifier = filename[:12]
-    index = state['index']
-    pub.send_frame(
-        identifier,
-        numpy_resampled_frame,
-        index,
-        pos_y,
-        pos_x
-    )
-    # pub.send_pyobj(output_event)
+    if state['start_sending_frames']:
+        # identifier, data, index, posy, posx, metadata
+        pos_x, pos_y = state['position']
+        filepath = state['metadata']['header']
+        filename = os.path.basename(filepath)
+        identifier = filename[:12]
+        index = state['index']
+        pub.send_frame(
+            identifier,
+            numpy_resampled_frame,
+            index,
+            pos_y,
+            pos_x
+        )
 
     # Here we zero out the exposures and masks for the next frame
     state['current_exposures'] = {dwell: [] for dwell in state['dwells']}
@@ -297,12 +285,14 @@ def finalize_frame(cxi_file, state, pub, config):
 
     output_position = t.zeros(3)
     output_position[:2] = t.as_tensor(state['position'])
-    
+
     file_handling.add_frame(cxi_file,
                             resampled_frame,
                             output_position,
                             mask=resampled_mask,
                             compression=config['compression'])
+
+    return numpy_resampled_frame
 
 
 def trigger_ptycho(state, config):
@@ -313,7 +303,7 @@ def trigger_ptycho(state, config):
         output_filename = make_output_filename(state)
         output_filename = '/'.join(output_filename.split('/')[5:])
         parameters = {
-            'path' : output_filename,
+            'path': output_filename,
         }
         try:
             run_deployment(name='Reconstruct from .cxi/reconstruct-from-cxi',
@@ -322,17 +312,17 @@ def trigger_ptycho(state, config):
         except:
             print('Failed to contact prefect')
 
+
 def process_stop_event(cxi_file, state, event, pub, config):
     if state['metadata'] is None:
         # This is just a backup check, the upstream logic *should* prevent
         # this from getting triggered in the absence of a start event
         print('Never got a start event, not processing')
         return
-    
+
     print('Processing stop event\n\n')
     finalize_frame(cxi_file, state, pub, config)
     pub.send_stop({})
-    # pub.send_pyobj(event)
 
 
 def process_emergency_stop(cxi_file, state, pub, config):
@@ -343,42 +333,94 @@ def process_emergency_stop(cxi_file, state, pub, config):
     print('Doing an emergency stop\n\n')
     finalize_frame(cxi_file, state, pub, config)
 
-    
 
-
-def run_data_accumulation_loop(cxi_file, state, event,
-                               sub, pub, config):
+def run_data_accumulation_loop(
+        cxi_file,
+        state,
+        event,
+        sub,
+        pub,
+        config
+):
     """Accumulates darks and frame data into a cxi file, until it completes
     """
-    
+
+    frames_for_illu_init = []
     while True:
         event = sub.recv_pyobj()
-        
+
         if event['event'].lower().strip() == 'start':
+            frames_for_illu_init.clear()
             process_emergency_stop(cxi_file, state, pub, config)
-            return event # pass the event back so we can start a new loop
+            return event  # pass the event back so we can start a new loop
 
         if event['event'].lower().strip() == 'stop':
             process_stop_event(cxi_file, state, event, pub, config)
             return None
-                    
+
         elif event['event'].lower().strip() == 'frame':
             if event['data']['ccd_mode'] == 'dark':
                 process_dark_event(cxi_file, state, event, config)
             else:
-                process_exp_event(cxi_file, state, event, pub, config)
+                frame = process_exp_event(cxi_file, state, event, pub, config)
+
+                if frame is not None:
+                    frames_for_illu_init.append(frame)
+                else:
+                    continue
+
+                n_frames_nominal = len(state['metadata_cxi']['translations'])
+                n_frames_to_init_illu = int(n_frames_nominal * config['dp_fraction_for_illumination_init'])
+                if len(frames_for_illu_init) == n_frames_to_init_illu:
+                    state['start_sending_frames'] = True
+
+                    illu, illu_mask = illumination_init.init_illumination(
+                        np.array(frames_for_illu_init),
+                        state['metadata_cxi']
+                    )
+
+                    state['metadata_cxi']['illumination_real'] = illu.real.tolist()
+                    state['metadata_cxi']['illumination_imag'] = illu.imag.tolist()
+                    state['metadata_cxi']['illumination_mask'] = illu_mask.tolist()
+
+                    pub.send_start(state['metadata_cxi'])
+
+                    time.sleep(1.0)
+
+                    # TODO: The index of the diffraction pattern is specified here as the index in the array.
+                    # TODO: This is a simplification, as it assumes that the diffraction patterns are received in order,
+                    # TODO: which is not necessarily true. However, left for now and can be fixed if the reconstruction
+                    # TODO: looks funny.
+                    for frame_idx, frame in enumerate(frames_for_illu_init):
+                        pos_x, pos_y = state['metadata_cxi']['translations'][frame_idx]
+                        filepath = state['metadata']['header']
+                        filename = os.path.basename(filepath)
+                        identifier = filename[:12]
+                        index = frame_idx
+                        pub.send_frame(
+                            identifier,
+                            frame,
+                            index,
+                            pos_y,
+                            pos_x
+                        )
+
+                        time.sleep(0.05)
+
+                    # TODO: write illumination and illumination mask into cxi file here.
+                    cxi_file.create_dataset('entry_1/instrument_1/source_1/illumination', data=illu)
+                    cxi_file.create_dataset('entry_1/instrument_1/source_1/probe_mask', data=illu_mask)
 
 
 def main(argv=sys.argv):
-
     # I'm just using this so the script has a help printout
     parser = argparse.ArgumentParser(
-        prog = 'process_live_data',
-        description = 'Runs a program which listens for raw data being emitted by pystxmcontrol. It assembles and preprocesses this data, saving the results inot .cxi files and passing the cleaned data on for further analysis.')
+        prog='process_live_data',
+        description='Runs a program which listens for raw data being emitted by pystxmcontrol. It assembles and preprocesses this data, saving the results inot .cxi files and passing the cleaned data on for further analysis.')
     args = parser.parse_args()
-    
+
     # TODO maybe add the ZMQ ports as args?
-    
+
     # Now we get the config info to read the ZMQ ports
     config = config_handling.get_configuration()
 
@@ -386,16 +428,14 @@ def main(argv=sys.argv):
     sub = context.socket(zmq.SUB)
     sub.connect(config['subscription_port'])
     sub.setsockopt(zmq.SUBSCRIBE, b'')
-    # pub = context.socket(zmq.PUB)
-    # pub.bind(config['broadcast_port'])
     pub = PreprocessorStream()
 
-    print('Listening for raw frames on',config['subscription_port'])
-    print('Broadcasting processed frames on',config['broadcast_port'])    
-    
+    print('Listening for raw frames on', config['subscription_port'])
+    print('Broadcasting processed frames on', config['broadcast_port'])
+
     start_event = None
     while True:
-                
+
         # This logic allows us to deal with cases where the 'stop' event
         # doesn't make it to the program. In that case, we need to keep the
         # 'start' event around to start the next file
@@ -413,11 +453,10 @@ def main(argv=sys.argv):
             config = config_handling.get_configuration()
             config_handling.summarize_config(config)
             state = make_new_state()
-            
+
             # Process start event returns a context manager for an open
             # cxi file with the appropriate metadata
-            with process_start_event(state, event, pub, config=config) \
-                 as cxi_file:
+            with process_start_event(state, event, pub, config=config) as cxi_file:
                 # This loop will read in darks and exp data until it gets
                 # a stop or start event, at which point it will return.
                 start_event = run_data_accumulation_loop(cxi_file, state, event, sub, pub, config)
@@ -426,7 +465,6 @@ def main(argv=sys.argv):
             # the file, to ensure that the file exists when the reconstruction
             # begins.
             trigger_ptycho(state, config)
-
 
 
 if __name__ == '__main__':
